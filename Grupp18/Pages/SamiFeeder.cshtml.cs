dfde15faca2d585f9235affada16f46a849131ad
artonb <146065746+artonb@users.noreply.github.com>
2024-02-09 14:20:02 +0100

    Initial

---
+++ Grupp18/Pages/SamiFeeder.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Grupp18.Pages
+{
+    public class SamiFeederModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
